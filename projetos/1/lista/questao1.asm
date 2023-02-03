a0698f6bccf693772ca05c2a54801436ede4cf5d
Julyanne Correia <julyanne.es@gmail.com>
2023-02-03 00:49:46 -0300

    problem: not printing the word, but the letter

--- projetos/1/lista/questao1.asm
+++ projetos/1/lista/questao1.asm
@@ -1,37 +1,61 @@
 .data
-	s:		.asciiz "aaa"
-	c1: 	.asciiz "a" 
-	c2: 	.asciiz "b"
-
+	s: .asciiz "Digite aqui uma frase: "
+	c1: .asciiz "Digite um caracter: "
+	c2: .asciiz "Agora digite outro caracter: "
+	space: .space 256
 .text
+
+.macro print (%string)
+	la $a0, %string
+	addi $v0, $0, 4
+	syscall
+.end_macro
+
+.macro read (%max_length)
+	li $v0, 8        # Lê o char c2 do usuario
+	la $a0, space
+	li $a3, %max_length
+	syscall
+.end_macro
 
 start:
 	# TODO: Leitura da String e Char do usuário
-	la	$s0, s
+	la	$a0, s
 
-	la	$s1, c1		# 
- 	lb	$s1, 0($s1)		# 
+	la	$a1, c1		# s1
+ 	lb	$a1, 0($a1)		# 
 
-	la	$s2, c2
-	lb	$s2, 0($s2)	# 
+	la	$a2, c2
+	lb	$a2, 0($a2)	# s2 
 	
-	add	$t0, $s0, $t0		# $t0 = $s0 + $t0
+	print(s)
+	read(256)
+
+	print(c1)
+	read(1)
+
+	print(c2)
+	read(1)
+
+	add	$t0, $a0, $t0		# a0$t0 = $s0 + $t0 Salva a entrada s em $t0
+	add $t1, $a1, $t1
+	add $t2, $a2, $t2
 
 loop_over_string:
-	lb	$t1, 0($t0)
+	lb	$t3, 0($t0)
 	
-	beq	$t1, $zero, exit	# if $t1 == $zero then goto exit
-	bne	$t1, $s1, not_equal	# if $t1 != $s1 then goto not_equal
-	sb	$s2, 0($t0)		# Substituição
+	beq	$t3, $zero, exit	# if $t1 == $zero then goto exit
+	bne	$t3, $t1, not_equal	# if $t1 != $s1 then goto not_equal
+	sb	$t2, 0($t0)		# Substituição
 
 not_equal:
 	addi	$t0, $t0, 1			# $t0 = $t0 + 1
-	sll	$t1, $0, 4			# $t1 = $t1 << 4
+	sll	$t3, $0, 4			# $t1 = $t1 << 4
 	j	loop_over_string
 
 exit:
 	addi	$v0, $0, 4		# system call #4 - print string
-	add	$a0, $s0, $zero		# $a0 = $s0 + $zero
+	add	$a0, $a0, $zero		# $a0 = $s0 + $zero
 	syscall						# execute
 
 	addi	$v0, $0, 10		# System call 10 - Exit
