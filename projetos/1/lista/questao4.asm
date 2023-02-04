ed6173b19357f04897d1b470766810ac8f0b7ce9
SteffanoP <steffanoxpereira@gmail.com>
2023-02-04 00:05:58 -0300

    Projeto - Lista: questão 04.c

--- projetos/1/lista/questao4.asm
+++ projetos/1/lista/questao4.asm
@@ -2,6 +2,8 @@
     string: .asciiz "Minha String"
     copia: .asciiz "String Minha"
     num: .word 4
+    str1: .asciiz "String"
+    str2: .asciiz "String"
 
 .text
 
@@ -31,6 +33,20 @@ main:
     addi	$v0, $0, 4		# system call #4 - print string
     syscall						# execute
 
+    # Print new line
+    addi	$a0, $0, 10			# $a0 = $0 + 10
+    addi $v0, $0, 11
+    syscall
+
+    #strcmp example
+    la		$a0, str1		# 
+    la		$a1, str2		# 
+    
+    jal		strcmp			# jump to strcpy and save position to $ra
+
+    addi	$a0, $v0, 0			# $t0 = $t1 + 0
+    addi	$v0, $0, 1		# system call #1 - print int
+    syscall						# execute
     j		exit				# jump to exit
 
 strcpy:
@@ -69,6 +85,30 @@ loop_over_num:
 finish_memcpy:
     sb		$zero, 0($t1)		# 
     addi	$v0, $a0, 0			# $v0 = $a0 + 0
+    jr		$ra					# jump to $ra
+
+strcmp:
+    addi	$t0, $a0, 0			# $t0 = $a0 + 0
+    addi	$t1, $a1, 0			# $t1 = $a1 + 0
+    addi	$v0, $zero, 0
+
+loop_over_strings:
+    lb		$t3, 0($t0)		# 
+    lb		$t4, 0($t1)		# 
+    bne		$t3, $t4, compare_greater	# if $t3 != $t4 then goto compare_greater
+    beq		$t3, $zero, finish_strcmp	# if $t3 == $zero then goto finish_strcmp
+
+    addi	$t0, $t0, 1			# $t0 = $a0 + 0
+    addi	$t1, $t1, 1			# $t1 = $a1 + 0
+    j		loop_over_strings	# jump to loop_over_strings
+    
+compare_greater:
+    addi $v0, $v0, 1
+    slt		$t3, $t3, $t4		# $t3 = ($t3 < $t4) ? 1 : 0
+    beq		$t3, $zero, finish_strcmp	# if $t3 == $zero then goto finish_strcmp
+    subi	$v0, $v0, 2			# $v0 = $v0 - 2
+
+finish_strcmp:
     jr		$ra					# jump to $ra
 
 exit:
