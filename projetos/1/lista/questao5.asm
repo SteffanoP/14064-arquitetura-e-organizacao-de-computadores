e898d305f2967ed4d5c4cb5dde614a2cffc468ae
SteffanoP <steffanoxpereira@gmail.com>
2023-02-04 14:36:34 -0300

    Projeto - Lista: questão 05 (incompleta)

---
+++ projetos/1/lista/questao5.asm
@@ -0,0 +1,35 @@
+.data
+    receiver_ready: .word 0xffff0000
+    receiver_data: .word 0xffff0004
+
+    transmitter_ready: .word 0xffff0008
+    transmitter_data: .word 0xffff000c
+
+.text
+
+main:
+    lw		$s0, receiver_ready		# 
+    lw		$s1, receiver_data		# 
+    lw		$s2, transmitter_ready	# 
+    lw		$s3, transmitter_data	# 
+    
+loop:
+    lb		$t0, 0($s0)		# 
+    bne		$t0, $zero, read_and_write_character	# if $t0 != $zero then goto read_character
+    j		loop			# jump to loop
+
+read_and_write_character:
+    lw		$t0, 0($s1)		# 
+    sw		$t0, 0($s3)		#
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+    addi	$t1, $t1, 1			# $t1 = $t1 + 1
+
+wait_display_to_write:
+    lb		$t0, 0($s2)		# 
+    beq		$t0, $zero, wait_display_to_write	# if $t0 == $zero then goto wait_display_to_write
+    j		loop				# jump to loop
+    
