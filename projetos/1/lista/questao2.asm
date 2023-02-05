ff567163e0d00cd8a5ae195ef60a397b25b253be
Julyanne Correia <julyanne.es@gmail.com>
2023-02-05 17:13:28 -0300

    fix: reading from user input

--- projetos/1/lista/questao2.asm
+++ projetos/1/lista/questao2.asm
@@ -1,9 +1,22 @@
 .data
-	string: .asciiz "Minha String"
+	string: .asciiz "Digite uma frase (max 40 char): "
+	space: .space 40
 
 .text
-	# TODO: Ler String do usuário
-	la		$a0, string		# 
+
+	li $v0, 4       # print string "Digite uma frase..."
+	la $a0, string
+	syscall
+
+	li $v0, 8       # read string entered by user
+	la $a0, space   # save space to store the string
+	li $a1, 40      # settle the max length
+	la $a0, string	# load string
+	add $t0, $a0, $t0    # $t0 = $a0 + $t0
+	syscall
+
+	addi $v0, $0, 0 # $v0 = $0 + 0 restore $v0 valeu to 0
+	 
 	jal		string_size		# jump to string_size and save position to $ra
 	addi	$t0, $v0, 0		# $t0 = $v0 + 0
 
@@ -19,7 +32,7 @@ string_size:
 loop_over_string:
 	lb		$t1, 0($t0)		# 
 	
-	beq		$t1, $zero, end_string	# if $t1 == $zero then goto end_string
+	beq		$t1, 10, end_string	# if $t1 == 10 then goto end_string (10 represents a new line because of the Enter button pressed)
 
 	addi	$v0, $v0, 1			# $t2 = $t2 + 1
 	addi	$t0, $t0, 1			# $t0 = $t0 + 1
