c9176a2781c6663abfeb04662607146eddf4784e
SteffanoP <steffanoxpereira@gmail.com>
2023-03-16 21:26:39 -0300

    Projeto 2 - Initial Commit

---
+++ projetos/2/mips.v
@@ -0,0 +1,7 @@
+module mips(in, out);
+	input wire in;
+	output wire out;
+	
+	assign out = !in;
+	
+endmodule
---
+++ projetos/2/ula.v
@@ -0,0 +1,9 @@
+
+
+module ula(In1, In2, OP, result, Zero_flag);
+	input wire [31:0] In1, In2;
+	input wire [3:0] OP;
+	output wire [31:0] result;
+	output wire Zero_flag;
+
+endmodule
