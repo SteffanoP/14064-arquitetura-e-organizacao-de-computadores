00d5c1474ec7ea90856d174ec807b563d361b963
Julyanne Correia <julyanne.es@gmail.com>
2023-03-21 20:51:39 -0300

    feat: pc module calling

--- projetos/2/mips.v
+++ projetos/2/mips.v
@@ -11,4 +11,18 @@ module mips(in, out);
 	wire Zero_flag;
 	ula mips_ula(In1, In2, OP, result);
 	
+	// PC MODULE
+	wire clock;
+	wire [31:0] PC; // contém o endereço atual
+	wire [31:0] nextPC; // conterá o próximo endereço (a atualização da soma)
+
+	Clock current_clock(clock);
+	PC pc_check(PC, nextPC, clock);
+
+	wire [31:0] pc_increment;
+	Counter pc_counter(nextPC, pc_increment); //talvez setar o 4 dentro da função seja melhor...
+
+	// INSTRUCTION MEMORY MODULE
+	wire [31:0] instruction;
+	i_mem current_instruction(nexPC, instruction);
 endmodule
