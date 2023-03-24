14ec8d9f4302cfb6c7f63af4626e59c6298683f1
hagafedra <haga.fedra@gmail.com>
2023-03-23 21:17:28 -0300

    Segunda versão regfile com Reset manual

--- projetos/2/regfile.v
+++ projetos/2/regfile.v
@@ -5,34 +5,49 @@ input [31:0] WriteData;
 input Clock, RegWrite, Reset;
 
 output [31:0] ReadData1, ReadData2;
-reg [31:0] ReadData1, ReadData2;
 
 reg [31:0] Reg[0:31];
 
-integer i;
+assign ReadData1 = Reg[ReadAddr1];
+assign ReadData2 = Reg[ReadAddr2];
 
-task rst;
-    begin
-       for(i = 0; i < 32; i = i+1)
-			Reg[i] = 0;
-    end
-endtask
-
-initial begin
-	rst;
-end
-
-always @(posedge Clock) begin
-	if(Reset)
-		rst;
+always @(posedge Clock or posedge Reset) begin
+	if(Reset) begin
+		Reg[0] <= 16'b0;  
+		Reg[1] <= 16'b0;  
+		Reg[2] <= 16'b0;  
+		Reg[3] <= 16'b0;  
+		Reg[4] <= 16'b0;  
+		Reg[5] <= 16'b0;  
+		Reg[6] <= 16'b0;  
+		Reg[7] <= 16'b0;
+		Reg[8] <= 16'b0;  
+		Reg[9] <= 16'b0;  
+		Reg[10] <= 16'b0;  
+		Reg[11] <= 16'b0;  
+		Reg[12] <= 16'b0;  
+		Reg[13] <= 16'b0;  
+		Reg[14] <= 16'b0;  
+		Reg[15] <= 16'b0;
+		Reg[16] <= 16'b0;  
+		Reg[17] <= 16'b0;  
+		Reg[18] <= 16'b0;  
+		Reg[19] <= 16'b0;  
+		Reg[20] <= 16'b0;  
+		Reg[21] <= 16'b0;  
+		Reg[22] <= 16'b0;  
+		Reg[23] <= 16'b0;
+		Reg[24] <= 16'b0;  
+		Reg[25] <= 16'b0;  
+		Reg[26] <= 16'b0;  
+		Reg[27] <= 16'b0;  
+		Reg[28] <= 16'b0;  
+		Reg[29] <= 16'b0;  
+		Reg[30] <= 16'b0;  
+		Reg[31] <= 16'b0;
+	end	
 	else begin
-		if(RegWrite == 1'b1)
-		Reg[WriteAddr] = WriteData;
-		else begin
-			ReadData1 = Reg[ReadAddr1];
-			ReadData2 = Reg[ReadAddr2];
-		end
-	end
+		if(RegWrite == 1'b1) Reg[WriteAddr] = WriteData;
+	end	
 end
-
 endmodule
