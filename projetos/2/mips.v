46e1d70f849dd406b080aab7bc1f425855d004e2
SteffanoP <steffanoxpereira@gmail.com>
2023-03-26 20:01:41 -0300

    refactor: differentiate `pc` wire and `PC` module

--- projetos/2/mips.v
+++ projetos/2/mips.v
@@ -17,10 +17,11 @@ module mips(clock, reset, pc, ula_result, data_mem);
 	
 	// PC MODULE
 	wire [31:0] nextPC; // conterá o próximo endereço (a atualização da soma)
-	PC pc_check(PC, nextPC, clock);
+	PC pc_check(pc, nextPC, clock);
 
-	wire [31:0] pc_increment; // Representará o resultado da soma do valor do PC
-	Counter pc_counter(nextPC, pc_increment); // Módulo para atualizar o valor do PC
+	// TODO: Implement pc_increment on MUX that will select the next PC
+	// wire [31:0] pc_increment; // Representará o resultado da soma do valor do PC
+	Counter pc_counter(nextPC, pc); // Módulo para atualizar o valor do PC
 
 	// INSTRUCTION MEMORY MODULE
 	wire [31:0] instruction;
