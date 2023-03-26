8175635b90b4faece933eec08a2bdda14a18f087
Julyanne Correia <julyanne.es@gmail.com>
2023-03-26 14:46:18 -0300

    fix: Clock module removed

--- projetos/2/mips.v
+++ projetos/2/mips.v
@@ -15,12 +15,10 @@ module mips(in, out);
 	wire clock;
 	wire [31:0] PC; // contém o endereço atual
 	wire [31:0] nextPC; // conterá o próximo endereço (a atualização da soma)
-
-	Clock current_clock(clock);
 	PC pc_check(PC, nextPC, clock);
 
-	wire [31:0] pc_increment;
-	Counter pc_counter(nextPC, pc_increment); //talvez setar o 4 dentro da função seja melhor...
+	wire [31:0] pc_increment; // Representará o resultado da soma do valor do PC
+	Counter pc_counter(nextPC, pc_increment); // Módulo para atualizar o valor do PC
 
 	// INSTRUCTION MEMORY MODULE
 	wire [31:0] instruction;
