8c331d1d4fe617d635e1f299d725de52a81cab25
SteffanoP <steffanoxpereira@gmail.com>
2023-03-26 20:49:19 -0300

    feat: implements branching for PC on MIPS

--- projetos/2/mips.v
+++ projetos/2/mips.v
@@ -1,6 +1,6 @@
 `include "counter.v"
 `include "i_mem.v"
-`include "mux_src.v"
+`include "mux.v"
 `include "pc.v"
 `include "regfile.v"
 `include "ula.v"
@@ -12,18 +12,23 @@ module mips(clock, reset, pc, ula_result, data_mem);
 	// ULA MODULE
 	reg [31:0] In1, In2;
 	reg [3:0] OP;
-	wire Zero_flag;
-	ula mips_ula(In1, In2, OP, ula_result);
+	wire ula_zero_flag;
+	ula mips_ula(In1, In2, OP, ula_result, ula_zero_flag);
 	
 	// PC MODULE
 	wire [31:0] nextPC; // conterá o próximo endereço (a atualização da soma)
 	PC pc_check(pc, nextPC, clock);
 
-	// TODO: Implement pc_increment on MUX that will select the next PC
-	// wire [31:0] pc_increment; // Representará o resultado da soma do valor do PC
-	Counter pc_counter(nextPC, pc); // Módulo para atualizar o valor do PC
+	wire [31:0] pc_increment; // Representará o resultado da soma do valor do PC
+	Counter pc_counter(nextPC, pc_increment); // Módulo para atualizar o valor do PC
 
 	// INSTRUCTION MEMORY MODULE
 	wire [31:0] instruction;
 	i_mem current_instruction(nextPC, instruction);
+
+	wire branch; //Saída do controle em caso de branching
+	wire branch_sel; //Atribuição em caso de branching do PC para outra instrução
+	assign branch_sel = branch & ula_zero_flag; //AND de branching
+	// Atribuição da próxima instrução do Program Counter (PC)
+	mux_32 pc_mux(pc_increment, ula_result, branch_sel, pc);
 endmodule
