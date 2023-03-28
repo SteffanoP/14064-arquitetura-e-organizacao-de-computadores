4db443c5a63efb3213bb2778f3a3be884951f5d5
SteffanoP <steffanoxpereira@gmail.com>
2023-03-28 10:59:06 -0300

    feat: branching control

--- projetos/2/mips.v
+++ projetos/2/mips.v
@@ -5,6 +5,7 @@
 `include "regfile.v"
 `include "ula.v"
 `include "sign_extend.v"
+`include "utils.v"
 
 module mips(clock, reset, pc, ula_result, data_mem);
 	input wire clock, reset;
@@ -31,9 +32,13 @@ module mips(clock, reset, pc, ula_result, data_mem);
 	wire [31:0] sign_extend_to_mux;
 	sign_extend mips_sign_extend(instruction[15:0], sign_extend_to_mux);
 
+	//Somador para branching
+	add32 branching(pc_increment, (sign_extend_to_mux << 2), add_branching_to_mux);
+	wire [31:0] add_branching_to_mux;
+
 	wire branch; //Saída do controle em caso de branching
 	wire branch_sel; //Atribuição em caso de branching do PC para outra instrução
 	assign branch_sel = branch & ula_zero_flag; //AND de branching
 	// Atribuição da próxima instrução do Program Counter (PC)
-	mux_32 pc_mux(pc_increment, ula_result, branch_sel, pc);
+	mux_32 pc_mux(pc_increment, add_branching_to_mux, branch_sel, pc);
 endmodule
