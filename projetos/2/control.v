1b6916e26a398618044d9d79ba352186b31246c0
SteffanoP <steffanoxpereira@gmail.com>
2023-03-28 21:51:38 -0300

    feat: create MIPS control module

---
+++ projetos/2/control.v
@@ -0,0 +1,81 @@
+module control (
+    opcode,
+    RegDst,
+    Branch,
+    MemRead,
+    MemtoReg,
+    ALUOp,
+    MemWrite,
+    ALUSrc,
+    RegWrite
+);
+    input wire [5:0] opcode;
+    output reg RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+    output reg [1:0] ALUOp;
+
+    always @(opcode) begin
+        case (opcode)
+            6'b000000: begin //add
+                RegDst = 1'b1;
+                Branch = 1'b0;
+                MemRead = 1'b0;
+                MemtoReg = 1'b0;
+                ALUOp = 2'b10;
+                MemWrite = 1'b0;
+                ALUSrc = 1'b0;
+                RegWrite = 1'b1;
+            end
+            6'b000111: begin //addi
+                RegDst = 1'b0;
+                Branch = 1'b0;
+                MemRead = 1'b0;
+                MemtoReg = 1'b0;
+                ALUOp = 2'b00;
+                MemWrite = 1'b0;
+                ALUSrc = 1'b1;
+                RegWrite = 1'b1;
+            end
+            6'b100011: begin //lw
+                RegDst = 1'b0;
+                Branch = 1'b0;
+                MemRead = 1'b1;
+                MemtoReg = 1'b1;
+                ALUOp = 2'b00;
+                MemWrite = 1'b0;
+                ALUSrc = 1'b1;
+                RegWrite = 1'b1;
+            end
+            6'b101011: begin //sw
+                RegDst = 1'b0;
+                Branch = 1'b0;
+                MemRead = 1'b0;
+                MemtoReg = 1'b0;
+                ALUOp = 2'b00;
+                MemWrite = 1'b1;
+                ALUSrc = 1'b1;
+                RegWrite = 1'b0;
+            end
+            6'b000100: begin //beq
+                RegDst = 1'b0;
+                Branch = 1'b1;
+                MemRead = 1'b0;
+                MemtoReg = 1'b0;
+                ALUOp = 2'b00;
+                MemWrite = 1'b0;
+                ALUSrc = 1'b0;
+                RegWrite = 1'b0;
+            end
+            default: begin //does nothing :)
+                RegDst = 1'b0;
+                Branch = 1'b0;
+                MemRead = 1'b0;
+                MemtoReg = 1'b0;
+                ALUOp = 2'b00;
+                MemWrite = 1'b0;
+                ALUSrc = 1'b0;
+                RegWrite = 1'b0;
+            end
+        endcase
+    end
+    
+endmodule
