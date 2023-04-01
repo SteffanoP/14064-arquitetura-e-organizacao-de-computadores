88ee575ce5ce83baf05f8c77cd5d397d89007181
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-04-01 10:20:50 -0300

    tornando circuito sequencial

--- projetos/2/d_mem.v
+++ projetos/2/d_mem.v
@@ -4,7 +4,7 @@ module d_mem #(parameter MEM_SIZE = 8) (address, writeData, readData, memWrite, 
    input wire [31:0] address; 				//endereço de memória para leitura e gravação
    input wire [31:0] writeData; 			//dados a serem escritos na memória
    input wire memWrite, memRead;			//sinal de controle para escrever na memória | sinal de controle para ler da memória
-   output reg [31:0] readData; 		//dados lidos da memória
+   output wire [31:0] readData; 		//dados lidos da memória
 	
 
    //declarar os elementos internos
@@ -16,10 +16,11 @@ module d_mem #(parameter MEM_SIZE = 8) (address, writeData, readData, memWrite, 
       if (memWrite) begin					//se memWrite == 1 -> grava os dados fornecidos no endereço indicado
          memory[address/(4)] = writeData;
       end
-
-   // Lógica de leitura
-	   readData = memRead ? memory[address/(4)] : 32'bz; // Se memRead == 1, ele lê os dados da memória no endereço dado pelo 'address', e armazena em 'readData'
-		// Caso contrário, atribui '32'bz' a 'readData'
+		
 	end
+	
+	// Lógica de leitura
+	   assign readData = memRead ? memory[address/(4)] : 32'bz; // Se memRead == 1, ele lê os dados da memória no endereço dado pelo 'address', e armazena em 'readData'
+		// Caso contrário, atribui '32'bz' a 'readData'
 	
 endmodule
