d202b15d62b1e9527237683cdfcf23de44c90a9f
SteffanoP <steffanoxpereira@gmail.com>
2023-04-16 12:02:37 -0300

    fix: read bytes instead of hexadecimal values

--- projetos/2/i_mem.v
+++ projetos/2/i_mem.v
@@ -13,7 +13,7 @@ module i_mem (address, i_out);
     // Inicializando o arquivo com as instruções MIPS
     // TODO: Completar arquivo com mais instruções
     initial begin
-        $readmemh("MIPS_teste.list", rom_mem);
+        $readmemb("tests/MIPS_teste.list", rom_mem);
     end
 
     // Leitura assíncrona. Apenas o address é considerado no always.
