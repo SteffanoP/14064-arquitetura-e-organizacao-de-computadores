39f7cd1d1695c69255434866f7d0fa360831fda5
SteffanoP <steffanoxpereira@gmail.com>
2023-04-16 21:39:52 -0300

    fix: i_memory position for rom_mem

--- projetos/2/i_mem.v
+++ projetos/2/i_mem.v
@@ -4,8 +4,8 @@ module i_mem (address, i_out);
     parameter address_data = 32;
 
     // Declaração de entradas e saída
-    input [address_data-1:0] address;
-    output reg [address_data-1:0] i_out;
+    input wire [address_data-1:0] address;
+    output wire [address_data-1:0] i_out;
 
     // Declaração da memória ROM
     reg [address_data-1:0] rom_mem [0:memory_size-1];
@@ -13,15 +13,13 @@ module i_mem (address, i_out);
     // Inicializando o arquivo com as instruções MIPS
     // TODO: Completar arquivo com mais instruções
     initial begin
-        $readmemb("tests/MIPS_teste.list", rom_mem);
+        $readmemb("tests/steffano_tests.list", rom_mem);
     end
 
     // Leitura assíncrona. Apenas o address é considerado no always.
     // Nesse caso, a saída recebe a instrução presente no índice dado
     // pelos 5 últimos bits do address. Então, esse índice referencia
     // à localização da instrução no arquivo da rom_mem 
-    always @(address) begin
-        i_out = rom_mem[address[4:0]];
-    end
+    assign i_out = rom_mem[address[address_data-1:2]];
     
 endmodule
