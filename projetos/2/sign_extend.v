8f553f55bb4e7ec6500165812992cc813a52025f
SteffanoP <steffanoxpereira@gmail.com>
2023-04-21 18:16:42 -0300

    fix: sign_extend actually signs the value

--- projetos/2/sign_extend.v
+++ projetos/2/sign_extend.v
@@ -2,5 +2,5 @@ module sign_extend (in,out);
     input wire [15:0] in;
     output wire [31:0] out;
 
-    assign out = (in[15] == 1'b1) ? {16'hffff, in} : {16'h0000, in};
+    assign out = $signed(in);
 endmodule
